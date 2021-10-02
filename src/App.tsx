9c33c1d92b49e34d055855fe3c4b3819941a1d63
Mathieu <mathieurousseau123@gmail.com>
2021-10-01 17:10:03 -0700

    Testing API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fetchUnsplash } from "./API";
+// Types
+import { IImage } from "./API";
 
-function App() {
+const App = () => {
+  const [unsplashImage, setUnspashImage] = useState("");
+
+  fetchUnsplash();
+
+  // .then((res) => {
+  //   setUnspashImage(res);
+  // });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img src={unsplashImage} className="App-image" alt="unsplash" />
+        <p>This picture is brought to you by: Unsplash.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
