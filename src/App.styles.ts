e25c83e370b1011263c13b5a2a0398e2b3934817
Mathieu <mathieurousseau123@gmail.com>
2021-10-01 19:22:42 -0700

    Implement styling for page

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,10 +1,12 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+import backgroundPic from "./images/keith-misner-unsplash.jpg";
+
 export const Wrapper = styled.div`
   display: block;
   margin-left: auto;
   margin-right: auto;
   margin-top: 2em;
-  width: 50%;
+  width: 40%;
 
   img {
     max-width: 600px;
@@ -14,6 +16,35 @@ export const Wrapper = styled.div`
   }
 
   p {
-    width: 80%;
+    margin-left: 20px;
+    color: white;
+    text-align: center;
+  }
+
+  Button {
+    margin-left: 14em;
+  }
+
+  a {
+    margin-left: 14.5em;
+    color: white;
+  }
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    // background-image: url(${backgroundPic});
+    // background-size: cover;
+    // background: radial-gradient(circle, white, black);
+    background-color: #837960;
+    background-image: linear-gradient(
+      to bottom, transparent, #837960
+    ), url(${backgroundPic});
+    background-repeat: no-repeat;
+    background-size: cover;
   }
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Button from "@mui/material/Button";
 // Types
 import { IImage } from "./API";
 // Styles
-import { Wrapper } from "./App.styles";
+import { Wrapper, GlobalStyle } from "./App.styles";
 
 const App = () => {
   const [unsplashImage, setUnspashImage] = useState("");
@@ -19,24 +19,28 @@ const App = () => {
   }, []);
 
   return (
-    <Wrapper>
-      <img src={unsplashImage} className="App-image" alt="unsplash" />
-      <p>
-        This picture is brought to you by Unsplash. To Get a new picture, click
-        the button below:
-      </p>
-      <Button variant="contained" onClick={newPicture}>
-        New Picture
-      </Button>
-      <a
-        className="unsplash-link"
-        href="https://unsplash.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Unsplash
-      </a>
-    </Wrapper>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <img src={unsplashImage} className="App-image" alt="unsplash" />
+        <p>
+          This picture is brought to you by Unsplash. To Get a new picture,
+          click the button below:
+        </p>
+        <Button variant="contained" onClick={newPicture}>
+          New Picture
+        </Button>
+        <br />
+        <a
+          className="unsplash-link"
+          href="https://unsplash.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Unsplash
+        </a>
+      </Wrapper>
+    </>
   );
 };
 
